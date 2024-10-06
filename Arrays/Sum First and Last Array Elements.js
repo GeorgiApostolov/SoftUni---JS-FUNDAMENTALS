11e673a3af6b815613c19052f69567ce66b8a792
GeorgiApostolov <74099975+GeorgiApostolov@users.noreply.github.com>
2024-10-06 14:19:58 +0300

    Arrays

---
+++ Arrays/Sum First and Last Array Elements.js
@@ -0,0 +1,8 @@
+function solve(arr) {
+    let firstEl = arr[0];
+    let lastEl = arr[arr.length-1];
+    console.log(firstEl+lastEl);
+}
+solve([20, 30, 40]);
+solve([10, 17, 22, 33]);
+solve([11, 58, 69]);
